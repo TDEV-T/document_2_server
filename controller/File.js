3700f2140fa76728e08bd27f63c382e4b86c818b
ExtraCB <ktgz14211771329@gmail.com>
2023-05-12 08:09:00 +0700

    success edit

--- controller/File.js
+++ controller/File.js
@@ -16,13 +16,22 @@ exports.getFileAll = async (req, res, next) => {
 exports.editFile = async (req, res, next) => {
   try {
     const fileUpdate = req.file;
-    const {id}
-    console.log(req.body);
-    res.json({
-      status: "done",
-      url: "http://localhost:3306",
-      fileUpdate,
-    });
+    const { id, type } = req.body;
+
+    const filter = { _id: id };
+    const updateFields = {};
+    updateFields[`files.${type}`] = fileUpdate.filename;
+
+    const update = { $push: updateFields };
+    const result = await File.updateOne(filter, update);
+
+    // res.json({
+    //   status: "done",
+    //   url: "http://localhost:3306",
+    //   fileUpdate,
+    // });
+    console.log(result);
+    res.send(result);
   } catch (err) {
     console.log(err);
     res.status(400).send(err);
