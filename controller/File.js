c5d61cddfa43b38c23562b64079ab3c4d7a59534
ExtraCB <ktgz14211771329@gmail.com>
2023-05-12 14:17:40 +0700

    success

--- controller/File.js
+++ controller/File.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 //Model
 const File = require("./../models/File");
+//fs
+const fs = require("fs");
 
 exports.getFileAll = async (req, res, next) => {
   try {
@@ -31,6 +33,46 @@ exports.editFile = async (req, res, next) => {
     //   fileUpdate,
     // });
     console.log(result);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+};
+
+exports.deleteFileSingle = async (req, res, next) => {
+  try {
+    const { id_object, filename, type } = req.body;
+
+    const filePath = `uploads/${filename}`;
+
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error("เกิดข้อผิดพลาด", err);
+        return;
+      }
+
+      console.log("ไฟล์ถูกลบแล้ว", filePath);
+    });
+
+    const filter = { _id: id_object };
+    const updateFields = {};
+    updateFields[`files.${type}`] = filename;
+
+    const update = { $pull: updateFields };
+    const result = await File.updateOne(filter, update);
+    console.log(result);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+};
+
+exports.deleteFile = async (req, res, next) => {
+  try {
+    console.log(req.query.id);
+    const result = await File.deleteOne({ _id: req.query.id });
     res.send(result);
   } catch (err) {
     console.log(err);
--- routes/file.js
+++ routes/file.js
@@ -3,11 +3,18 @@ var router = express.Router();
 //auth
 const { auth } = require("./../middleware/Auth");
 //controler
-const { getFileAll, editFile } = require("./../controller/File");
+const {
+  getFileAll,
+  editFile,
+  deleteFileSingle,
+  deleteFile,
+} = require("./../controller/File");
 //middleware
 const { upload } = require("./../middleware/uploadfile");
 
 router.post("/getFileAll", auth, getFileAll);
 router.post("/editFileImg", auth, upload.single("file"), editFile);
+router.post("/deleteFileSingle", auth, deleteFileSingle);
+router.get("/delete", deleteFile);
 
 module.exports = router;
